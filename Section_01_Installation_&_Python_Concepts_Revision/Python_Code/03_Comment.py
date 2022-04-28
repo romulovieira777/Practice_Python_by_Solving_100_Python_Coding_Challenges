666a5513bbd3f0cc9eddf1dbaeb377f7d7322e5c
Romulo Vieira <romulo.vieira777@gmail.com>
2022-04-28 10:03:47 -0300

    Installation & Python Concepts Revision

---
+++ Section_01_Installation_&_Python_Concepts_Revision/Python_Code/03_Comment.py
@@ -0,0 +1,9 @@
+# This is a comment
+print(3 + 4)
+
+"""
+This are the comments
+This will no relationship with our code
+comment only
+"""
+print("Hello World, Python!")
---
+++ Section_01_Installation_&_Python_Concepts_Revision/Python_Code/04_Variables.py
@@ -0,0 +1,19 @@
+x = 5
+y = 6
+print(x * y)
+
+
+a, b = 5, 6
+print(a * b)
+
+
+c = "My name is Felicity Smoke!"
+print(c)
+
+
+d = 'a'
+print(d)
+
+
+e = 5.34
+print(e)
