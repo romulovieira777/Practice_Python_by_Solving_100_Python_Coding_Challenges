7c9da598224ba81eb641bf670294c069c67e7121
Romulo Vieira <romulo.vieira777@gmail.com>
2022-05-12 10:13:00 -0300

    Exercise 26 - 50

---
+++ Section_03_Exercise_26_50/47_Exercise.py
@@ -0,0 +1,27 @@
+"""
+Exercise No. 47
+
+Write a function that returns True if two arrays, when combined, form a consecutive sequence. A consecutive sequence is
+a sequence without any gaps in the integers, e.g. 1, 2, 3, 4, 5 is a consecutive sequence, but 1, 2, 4, 5 is not.
+
+Examples:
+    consecutive_combo([7, 4, 5, 1], [2, 3, 6]) -> True
+
+    consecutive_combo([1, 4, 6, 5], [2, 7, 8, 9]) -> False
+
+    consecutive_combo([1, 4, 5, 6], [2, 3, 7, 8, 10]) -> False
+
+    consecutive_combo([44, 46], [45]) -> True
+
+Notes:
+    - The input lists will have unique values.
+    - The input lists can be in any order.
+"""
+
+
+def consecutive_combo(lst1, lst2):
+    lst3 = lst1 + lst2
+    return max(lst3) - min(lst3) == len(lst3) - 1
+
+
+print(consecutive_combo([44, 46], [45]))
---
+++ Section_03_Exercise_26_50/48_Exercise.py
@@ -0,0 +1,28 @@
+"""
+Exercise No. 48
+
+Write a function that takes a positive integer num and calculates how many dots exist in a pentagonal shape around the
+center dot on the Nth iteration.
+
+In the image below you can see the first iteration is only a single dot. On the second, there are 6 dots. On the bird,
+there are 16 dots, and on the fourth there are 31 dots.
+
+Return the number of dots that exist in the whole pentagonal on the Nth iteration.
+
+Examples:
+    pentagonal(1) -> 1
+
+    pentagonal(2) -> 6
+
+    pentagonal(3) -> 16
+
+    pentagonal(8) -> 141
+"""
+
+
+def pentagonal(num):
+    if num == 1: return 1
+    return pentagonal(num - 1) + 5 * (num - 1)
+
+
+print(pentagonal(8))
