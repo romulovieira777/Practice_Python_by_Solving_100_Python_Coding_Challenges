7dd5018fe6f06a5950d1181daef2c8dfdaa16be0
Romulo Vieira <romulo.vieira777@gmail.com>
2022-05-13 10:30:55 -0300

    Exercise 26 - 50

---
+++ Section_03_Exercise_26_50/50_Exercise.py
@@ -0,0 +1,27 @@
+"""
+Exercise No. 50
+
+Create a function that takes a string as an argument and returns the Morse code equivalent.
+
+Example:
+    econde_morse("EDABBIT CHALLENGE") -> ". -.. .- - .... - ... .. -  -.-. .... .- .-.."
+
+    enconde_morse("HELP ME !") -> ".... . .-.. .--.   -- .  -.-.--"
+
+This is dictionary can be used for coding:
+    char_to_dots = {
+        'A':, 'B':, 'C':, 'D':, 'E':, 'F':,  'G':, 'H':, 'I':, 'J':, 'L':, 'M':, 'N':, 'O':, 'P':, 'Q';, 'R':, 'S':,
+        'T':, 'U':, 'V':, 'W':, 'X' 'Y' 'Z':,'
+    }
+
+
+"""
+
+
+
+
+
+
+
+
+
