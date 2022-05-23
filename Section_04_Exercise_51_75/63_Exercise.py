55d58e061284e3e3c2f90e07021cca8145e5268e
Romulo Vieira <romulo.vieira777@gmail.com>
2022-05-23 10:07:54 -0300

    Exercise 51 - 75

--- Section_04_Exercise_51_75/63_Exercise.py
+++ Section_04_Exercise_51_75/63_Exercise.py
@@ -1,17 +1,39 @@
 """
 Exercise No. 63
 
+Given a list of integers, return the smallest positive integer not present in the list.
 
+Here is a representative example. Consider the list:
+    [-2, 6, 4, 5, 7, -1, 7, 1, 3, 6, 6, -2, 9, 10, 2, 2]
+After reordering, the list becomes:
+    [-2, -2, -1, 1, 2, 2, 3, 4, 5, 6, 6, 6, 7, 7, 9, 10]
+... from which we see that the smallest missing positive integer is 8.
 
+Examples:
+    min_miss_pos([-2, 6, 4, 5, 7, -1, 1, 3, 6, -2, 9, 10, 2, 2]) -> 8
+    # After sorting, list becomes [-2, -2, -1, 1, 2, 2, 3, 4, 5, 6, 6, 7, 9, 10]
+    # So the smallest missing positive integer is 8
 
+    min_miss_pos([5, 9, -2, 0, 1, 3, 9, 3, 8, 9]) -> 2
+    # After sorting, list becomes [-2, 0, 1, 3, 3, 5, 8, 9, 9, 9]
+    #So the smallest missing positive integer is 2
 
+    min_miss_pos([0, 4, 4, -1, 9, 4, 5, 2, 10, 7, 6, 3, 10, 9]) -> 1
+    # After sorting, list becomes [-1, 0, 2, 3, 4, 4, 4, 5, 6, 7, 9, 9, 10, 10]
+    # So the smallest missing positive integer is 1
 
+Notes:
+    For the sake of clarity, recall that 0 is not considered to be a positive number.
 """
 
 
+def min_miss_pos(lst):
+    i = 1
+    while True:
+        if not i in lst:
+            return i
+        i += 1
+    return i
 
 
-
-
-
-
+print(min_miss_pos([-2, 6, 4, 5, 7, -1, 1, 3, 6, -2, 9, 10, 2, 2]))
--- Section_04_Exercise_51_75/64_Exercise.py
+++ Section_04_Exercise_51_75/64_Exercise.py
@@ -1,17 +1,35 @@
 """
 Exercise No. 64
 
+Give an integer, return a new integer according to the rules below:
+    - Split the number into groups of two digits numbers. If the number has an odd number of digits, return "invalid".
+    - For each group of two digits numbers, concatenate the last digit to a new string the same number of times as the
+    value of the first digit.
+    - Return the result as an integer.
+        look_and_say(3132) -> 111222
 
+        # By reading the number digit by digit, you get three "1" and three "2".
+        # Therefore, you put three ones and three two's together.
+        # Remember to return an integer.
+Examples:
+    look_and_say(95) -> 555555555
 
+    look_and_say(1213141516171819) -> 23456789
 
+    look_and_say(120520) -> 200
 
+    look_and_say(231) -> "invalid"
 
+Notes:
+    - Note that the number 0 can be included (see example #3).
 """
 
 
+def look_and_say(n):
+    n = str(n)
+    if len(n) % 2:
+        return 'invalid'
+    return int(''.join(int(n[i]) * n[i + 1] for i in range(0, len(n), 2)))
 
 
-
-
-
-
+print(look_and_say(231))
