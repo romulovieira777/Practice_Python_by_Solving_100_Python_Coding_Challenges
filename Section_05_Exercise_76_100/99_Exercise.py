f203210e799bc020ccf0d7da82219497714f3674
Romulo Vieira <romulo.vieira777@gmail.com>
2022-06-20 12:16:11 -0300

    Exercise 76 - 100

--- Section_05_Exercise_76_100/99_Exercise.py
+++ Section_05_Exercise_76_100/99_Exercise.py
@@ -1,7 +1,13 @@
 """
 Exercise No. 99
 
+Create a function that will increment avery time format found in a string (for example 00:42:33,120) by a specific time
+(for example 00:00:30,550). The result between the examples is 00:43:03,670.
 
+Rules:
+    - The function must take 2 arguments: a string from where to search time format and a string that respects the
+    format hours:minutes:seconds,milliseconds.
+    -
 """
 
 
